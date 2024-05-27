6a0f21aad0bcfa7a3346f09f8b0c75f353307d79
Bilguun <bilguun1324@gmail.com>
2024-05-27 11:59:55 +1000

    fixed framer motion erro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,3 @@
-import colors from 'tailwindcss/colors';
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 /** @type {import('tailwindcss').Config} */
@@ -6,12 +5,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
 		},
 	},
 	plugins: [starlightPlugin()],
